401c6cd4803e46e2c359606bd8351b3b2369216b
Yury Romanov <support@purptree.com>
2016-10-05 00:46:47 +0300

    Add form to template. Add/modify some styles.

--- template.php
+++ template.php
@@ -7,5 +7,14 @@
 </head>
 <body>
     <h2><?php echo $page->title ?></h2>
+
+    <div id="message-board"></div>
+    <div id="form">
+      <form action="index.php" method="get"><br />
+        <input name="name" type="text" placeholder="User" /><br />
+        <textarea name="comment"></textarea><br />
+        <input type="submit" />
+      </form>
+    </div>
 </body>
 </html>
