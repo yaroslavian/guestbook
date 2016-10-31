8c56846c035bac4a7cc83cd78ccc7bca9f9beb64
Yury Romanov <support@purptree.com>
2016-10-31 14:26:52 +0300

    Hotfix. Addition to previous commit

---
+++ js/lib.js
@@ -0,0 +1,87 @@
+;var page = (function(){
+////
+var page = {
+  lastId : 0,
+  requestedId: 0,
+
+  getMessages: function(id) {
+    var i, res,
+        messages = [],
+        url = 'modules/get-messages.php';
+        if(id) url+=('?id='+id);
+
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", url);
+    ajax.send();
+
+    var that = this;
+    ajax.onload = function() {
+      res = JSON.parse(ajax.responseText);
+
+      messages = res['messages'];
+
+      that.lastId = messages[messages.length-1]['id'];
+
+      //render
+      for(i=0;i<messages.length;i++) {
+        document.getElementById('message-board').appendChild(
+          function(){
+            var div = document.createElement('div');
+            div.classList.add('message-block');
+            div.innerHTML =	messages[i]['id'] + ':<br /><br />' +
+                            messages[i]['user'] + ':<br /><br />' +
+                            messages[i]['message'] + '<br /><br />' +
+                            messages[i]['date'];
+            return div;
+          }());
+        }
+    };
+  },
+
+  sendMessage: function(user, message) {
+    message = message.replace(/[,-_.!~*'()]/g, '\\$&');
+    user = encodeURIComponent(user);
+    message = encodeURIComponent(message);
+    var url = 'modules/send-message.php?user='+user+'&message='+message;
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", url);
+    ajax.send();
+  },
+
+  onScrollLoadMessages: function(){
+    if(document.body.clientHeight < window.scrollY + window.innerHeight) {
+      if(this.requestedId !== this.lastId) {
+        this.getMessages(this.lastId);
+        this.requestedId = this.lastId;
+      }
+    }
+  },
+
+  addScrollTopButton: function(){
+    var button = document.createElement('div');
+    button.id = 'scroll-top';
+    button.innerHTML= 'Scroll top';
+
+    var scrollTop = function(){
+		  if(window.scrollY >= 5) {
+		    window.scroll(0,window.scrollY-(scrollY/10));
+		    setTimeout(scrollTop,10);
+		  } else window.scroll(0,0);
+		};
+
+    button.onclick = scrollTop;
+
+    document.body.appendChild(button);
+
+    return function(){
+      if(window.scrollY > window.innerHeight*2/3) button.style.display = 'block';
+      else button.style.display = 'none';
+    };
+  }
+
+};
+
+return page;
+
+////
+}());
