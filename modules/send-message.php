8c51e223ba8abe71ce5d64e35f30b8961d74c61f
Yury Romanov <support@purptree.com>
2016-11-02 15:50:58 +0300

    Add message validation in send-message.php.

--- modules/send-message.php
+++ modules/send-message.php
@@ -1,13 +1,18 @@
 <?php
-  include '../lib.php';
-  session_start();
-  error_log($_SESSION['username']);
-  # Check for $_GET request to send new message. If ok, write it to the DB
-  isset($_GET['message']) && write_message((isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest'), $_GET['message'], $page);
+include '../lib.php';
+session_start();
 
-  function write_message($user, $message, $page){
-    $query = 'INSERT INTO `messages` (`user`,`message`) VALUES  (\''.$user.'\',\''.$message.'\')';
-    $res = $page->query_db($query);
-  }
+# Check for $_GET request to send new message. If ok, validate and write it to the DB
+isset($_GET['message']) &&
+  write_message(
+    (isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest'),
+    $_GET['message'],
+    $page
+  );
+
+function write_message($user, $message, $page){
+  preg_match('/^.{1,100}$/s', $message) &&
+    $page->query_db('INSERT INTO `messages` (`user`,`message`) VALUES  (\''.$user.'\',\''.$message.'\')');
+}
 
 ?>
