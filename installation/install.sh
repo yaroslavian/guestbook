e699e0ffdf5304549431fec2540ede895a01cffc
Yury Romanov <support@purptree.com>
2016-11-05 23:50:21 +0300

    Add ability to set the database access data from user input directly during installation.

--- installation/install.sh
+++ installation/install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
+#Should be runned from project_root/installation/
 
-DB_SERVER=database_server
-DB_NAME=database_name
-DB_USER=database_user
-DB_PASS=database_password
+read -p "DB SERVER: " DB_SERVER
+read -p "DB NAME: " DB_NAME
+read -p "DB USER: " DB_USER
+read -p "DB PASSWORD " DB_PASS
 
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < DB-dump.sql;
 
-echo "<?php 
-	\$config = array( 
-		'db' => array( 
-			'server' => '$DB_SERVER', 
+echo "<?php
+	\$config = array(
+		'db' => array(
+			'server' => '$DB_SERVER',
 			'name' => '$DB_NAME',
 			'user' => '$DB_USER',
-			'pass' => '$DB_PASS' 
-		) 
-	); 
-?>" > ../config.php
-
+			'pass' => '$DB_PASS'
+		)
+	);
+?>" > ../config1.php
